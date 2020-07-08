4bc2fe1dc0c93da1096b14a01ea8bd098d73cfdc
polymer <muhammetpolat.87@gmail.com>
2020-07-08 19:32:46 +0300

    If statement'lı 1. Assignment kodunu içeren dosya oluşturularakpyton kodu yazıldı

---
+++ Assignment_1.py
@@ -0,0 +1,6 @@
+password = "qwert1234"
+name = input("Enter your name please :")
+if name == "muhammet" or name == "Muhammet" or name == "MUHAMMET":
+    print("Hello, {}! The password is : {}".format(name, password))
+else:
+    print("Hello, {}! See you later.".format(name))
