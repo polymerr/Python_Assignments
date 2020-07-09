8bfda84ac4817ab0c79fd29f7d0fe78bd9c0d703
polymer <muhammetpolat.87@gmail.com>
2020-07-09 19:03:26 +0300

    A litte change made on Assignment-1

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,6 +1,6 @@
 password = "qwert1234"
-name = input("Enter your name please :")
-if name == "muhammet" or name == "Muhammet" or name == "MUHAMMET":
+name = input("Enter your name please :").title().strip()
+if name == "Muhammet":
     print("Hello, {}! The password is : {}".format(name, password))
 else:
     print("Hello, {}! See you later.".format(name))
